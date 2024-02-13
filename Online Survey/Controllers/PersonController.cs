5631e31c4db86a1bba86fc784976609a42908de9
fatihhidiroglu <fatih.hidiroglu@hotmail.com>
2024-02-13 12:30:54 +0300

    model eklendi ve create işlemi için db kayıdı yapılması sağlandı.

--- Online Survey/Controllers/PersonController.cs
+++ Online Survey/Controllers/PersonController.cs
@@ -1,3 +1,5 @@
+using Online_Survey.Models;
+using System;
 using System.Diagnostics;
 using System.Web.Mvc;
 
@@ -5,15 +7,29 @@ namespace Net_Survey.Controllers
 {
     public class PersonController : Controller
     {
+        SurveyEntities db = new SurveyEntities();
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Create()
+        public ActionResult Create(Person person)
         {
-            return View();
+            // Method çaðrýrýlýnca hemen ekleme yapmamasý için þart eklendi.
+            if (person.NameSurname != null)
+            {
+                person.CreateDate = DateTime.Now;
+                person.CreateBy = "System";
+
+                db.Person.Add(person);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View();
+            }
         }
-        
+
     }
 }
